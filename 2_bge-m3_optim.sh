816d9b65b61fad9921b3cbb1c67db6e9617bae33
alrazz-n <alireza.razzaghi.utu@gmail.com>
2025-12-30 12:17:24 +0200

    Hyper-param search
    Hyper serach with Optuna for multilingual, zero-shot and base setting for both XLMR and bge-m3

---
+++ 2_bge-m3_optim.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#SBATCH --job-name=bge-m3_optim
+#SBATCH --account=project_2002026
+#SBATCH --partition=gpusmall
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=16
+#SBATCH --mem=128G
+#SBATCH --time=08:00:00
+#SBATCH --gres=gpu:a100:1
+#SBATCH --output=bge-m3_optim_%j.out
+#SBATCH --error=bge-m3_optim_%j.err
+
+# 1. Load modules
+module purge
+module use /appl/local/csc/modulefiles
+module load pytorch/2.6
+
+# 2. Activate venv
+source cleanlab-venv/bin/activate
+
+# 4. Run script
+python3 2_bge-m3_optim.py
