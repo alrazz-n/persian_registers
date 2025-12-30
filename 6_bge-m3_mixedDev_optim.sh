ff9461093cd964459b8c96f07559f51879d05a58
alrazz-n <alireza.razzaghi.utu@gmail.com>
2025-12-30 13:02:33 +0200

    Mixed dev for multilingual setting
    Mixing other languages in dev on the multilingual setting

---
+++ 6_bge-m3_mixedDev_optim.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#SBATCH --job-name=bge-m3_mixedDev_optim
+#SBATCH --account=project_2002026
+#SBATCH --partition=gpusmall
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=16
+#SBATCH --mem=256G
+#SBATCH --time=12:00:00
+#SBATCH --gres=gpu:a100:1
+#SBATCH --output=bge-m3_optim_mixedDev_%j.out
+#SBATCH --error=bge-m3_optim_mixedDev_%j.err
+
+# 1. Load modules
+module purge
+module use /appl/local/csc/modulefiles
+module load pytorch/2.6
+
+# 2. Activate venv
+source cleanlab-venv/bin/activate
+
+# 4. Run script
+python3 6_bge-m3_mixedDev_optim.py
